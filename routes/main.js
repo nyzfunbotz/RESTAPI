eefa56900dcdb74a6b6bb7f93d10cb13f8044ddc
nyzfunbotz <94772209+nyzfunbotz@users.noreply.github.com>
2022-06-29 17:08:48 +0800

    Add files via upload

--- routes/main.js
+++ routes/main.js
@@ -6,4 +6,7 @@ var router = express.Router();
 router.get('/', (req, res) => {
     res.sendFile(__path + '/views/index.html')
 })
+router.get('/love', (req, res) => {
+    res.sendFile(__path + '/views/love.html')
+})
 module.exports = router
